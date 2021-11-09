03f483cf3b694f12e8a1e31c08496f592f975b07
zhulin211 <zhulin211@gmail.com>
2021-11-09 15:24:34 -0500

    Initial commit

---
+++ P2test1.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name, age, pay=0, job=None):
+        self.name = name
+        self.age = age
+        self.pay = pay
+        self.job = job
+qinke = Person('Qinke', 42, 3000)
+ma = Person('MaHaibo', 39, 2000)
+print(qinke.name)
+print(ma.pay)
+
